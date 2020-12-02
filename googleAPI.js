c3ccdae75778a8615be24509aaccba495e69c76d
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2020-12-01 19:38:10 -0600

    fixed url to show results to actual user location

--- googleAPI.js
+++ googleAPI.js
@@ -1,34 +1,34 @@
 
-//remove https://cors-anywhere.herokuapp.com/ from the url in the query url
 
 //add a function to arrange the list of food trucks to the nearest one to the user. prepend style.
 
+
+
 //add function to get location and install it into the function.
-
-
 var userPosition;
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  }
-}
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
 function showPosition(position) {
-  userPosition = {
-  lat: position.coords.latitude,
-  lon: position.coords.longitude
-  }
-  console.log(userPosition);
-  
-  userPosition.append();
-  $("#map").append(userPosition);
+    userPosition = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }
+    console.log(userPosition);
+    googleApi()  
 }
 
 getLocation();
 
 function googleApi() {
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=food+trucks&location=29.4241,-98.4936&radius=10000&key=AIzaSyBL-L9x6O3SIMHJkubbfPAPXsr_a1nx3EM";
+    //remove https://cors-anywhere.herokuapp.com/ from the url in the query url
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=food+trucks&location="+userPosition.lat+","+userPosition.lon+"&radius=10000&key=AIzaSyBL-L9x6O3SIMHJkubbfPAPXsr_a1nx3EM";
     var apiKey = "AIzaSyBL-L9x6O3SIMHJkubbfPAPXsr_a1nx3EM";
+    
+    //api call
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -37,14 +37,14 @@ function googleApi() {
         console.log(response);
         let map;
     
+    //function used to apply a marker on results
         function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
         zoom: 2,
-        center: new google.maps.LatLng(userPosition[0], userPosition[1]),
+        center: new google.maps.LatLng(coords[1], coords[0]),
         mapTypeId: "terrain",
     });
-    // Loop through the results array and place a marker for each
-    // set of coordinates.
+    // Loop through the results array and place a marker for each set of coordinates.
     }
     const eqfeed_callback = function (results) {
     for (let i = 0; i < results.features.length; i++) {
@@ -59,7 +59,7 @@ function googleApi() {
         });
         
     }
-googleApi ()
+//googleApi ()
 
 
 
