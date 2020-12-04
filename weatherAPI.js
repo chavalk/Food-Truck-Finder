43e4c4a54284c4d143be419e2f46087caf90fbf4
chavalk <chavalk@hotmail.com>
2020-12-03 23:45:00 -0600

    Replaced method to get user location in order to be able to get location when not connected to Wi-Fi.

--- weatherAPI.js
+++ weatherAPI.js
@@ -8,29 +8,35 @@ var apiKey = "391826fb0aefc3ce7776fffd2278448a";
 
 // Variables
 var city = "";
+var lat = 0;
+var lon = 0;
 var weather = "";
+var userPosition;
 
-// Function to get city from IP address
-function getCity(){
-
-    // URL for API to get city
-    var ipApi = "https://freegeoip.app/json/";
-
-    // Call to API
-    $.ajax({
-        url: ipApi,
-        method: "GET"
-    }).then(function(response){
-        city = response.city;
-        getCurrentWeather(city);
-    })
+// Function to get location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+function showPosition(position) {
+    userPosition = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }
+    lat = userPosition.lat;
+    lon = userPosition.lon;
+    getCurrentWeather(lat, lon);  
 }
 
+// Function call
+getLocation();
+
 // Function to get weather
-function getCurrentWeather(city){
+function getCurrentWeather(lat, lon){
     
     // URL to get current weather from API
-    var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    var currentURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
     // Call to API
     $.ajax({
@@ -38,18 +44,15 @@ function getCurrentWeather(city){
         method: "GET"
     }).then(function (response) {
         weather = response.weather[0].main;
-        if(weather == "Clear"){
+        if(weather == "Clear" || weather == "Clouds"){
             conditionGreen.removeClass("is-hidden");
-        } else if(weather == "Rain"){
+        } else if(weather == "Rain" || weather == "Drizzle"){
             conditionYellow.removeClass("is-hidden");
         } else{
             conditionRed.removeClass("is-hidden");
         }
     })
 }
-
-// Function call
-getCity();
 
 // Add functionality to modal button
 $("#launchGreen").click(function() {
